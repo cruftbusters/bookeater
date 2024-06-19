98771dca38d5b49edfb9afb721329e9a3fafae6a
tyler <tyler@cruftbusters.com>
2024-06-19 15:11:08 -0700

    edit transaction note

--- todo.md
+++ todo.md
@@ -5,6 +5,7 @@ This file is an ephemeral queue of granules of work.
 ## items
 
 * ~~create transaction~~
+* ~~edit transaction note~~
+* edit transaction note free text
 * edit transaction date
-* edit transaction note
 * add credit and debit to transaction
--- webapp/src/app.rs
+++ webapp/src/app.rs
@@ -4,23 +4,41 @@ use yew::prelude::*;
 #[derive(Clone, PartialEq)]
 struct Transaction {
     id: Uuid,
+    note: AttrValue,
 }
 
 #[function_component(App)]
 pub fn app() -> Html {
-    let transactions = use_state(|| vec![]);
+    let transactions: UseStateHandle<Vec<Transaction>> = use_state(|| vec![]);
+    let on_update = Callback::from({
+        let transactions = transactions.clone();
+        move |update: Transaction| {
+            let mut results = vec![];
+            for transaction in transactions.to_vec() {
+                if transaction.id == update.id {
+                    results.push(update.clone());
+                } else {
+                    results.push(transaction.clone());
+                }
+            }
+            transactions.set(results);
+        }
+    });
     let on_create = {
         let transactions = transactions.clone();
         move |_| {
             let mut value = transactions.to_vec();
-            value.push(Transaction { id: Uuid::new_v4() });
+            value.push(Transaction {
+                id: Uuid::new_v4(),
+                note: "".into(),
+            });
             transactions.set(value);
         }
     };
     html! {
         <main>
             <h1>{ "Transactions" }</h1>
-            { transactions.iter().map(|transaction| html! { <TransactionView transaction={transaction.clone()} /> }).collect::<Html>() }
+            { transactions.iter().map(|transaction| html! { <TransactionView on_update={on_update.clone()} transaction={transaction.clone()} /> }).collect::<Html>() }
             <div>
                 <button onclick={on_create}>{ "create" }</button>
             </div>
@@ -31,10 +49,29 @@ pub fn app() -> Html {
 #[derive(Properties, PartialEq)]
 pub struct TransactionViewProps {
     transaction: Transaction,
+    on_update: Callback<Transaction>,
 }
 
 #[function_component(TransactionView)]
 pub fn transaction_view(props: &TransactionViewProps) -> Html {
-    let TransactionViewProps { transaction } = props;
-    html! { <div>{ transaction.id }</div> }
+    let TransactionViewProps {
+        on_update,
+        transaction,
+    } = props;
+    let on_update = Callback::from({
+        let mut transaction = transaction.clone();
+        transaction.note = "updated".into();
+
+        let on_update = on_update.clone();
+        move |_| {
+            on_update.emit(transaction.clone());
+        }
+    });
+    html! {
+        <div>
+            <div>{ "id: " }{ &transaction.id }</div>
+            <div>{ "note: " }{ &transaction.note }</div>
+            <button onclick={on_update.clone()}>{ "update" }</button>
+        </div>
+    }
 }
