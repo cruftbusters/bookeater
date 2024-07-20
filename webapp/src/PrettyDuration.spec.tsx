126d6005b1144f6c95e1bd967ad82d9a7211f8a4
arctair <arctair>
2024-07-19 21:58:51 -0700

    pretty duration

---
+++ webapp/src/PrettyDuration.spec.tsx
@@ -0,0 +1,31 @@
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom'
+import {PrettyDuration} from "./PrettyDuration.tsx";
+
+describe(PrettyDuration, () => {
+    test("hours", () => {
+        render(<PrettyDuration seconds={60 * 60 * 6}/>)
+
+        expect(screen.getByText('6h')).toBeInTheDocument()
+    })
+    test("hours and minutes", () => {
+        render(<PrettyDuration seconds={60 * 60 * 4.33}/>)
+
+        expect(screen.getByText('4h20m')).toBeInTheDocument()
+    })
+    test("quarter hour", () => {
+        render(<PrettyDuration seconds={60 * 60 * 0.25}/>)
+
+        expect(screen.getByText('15m')).toBeInTheDocument()
+    })
+    test("less than five minutes", () => {
+        render(<PrettyDuration seconds={60 * 4}/>)
+
+        expect(screen.getByText('4m')).toBeInTheDocument()
+    })
+    test("less than five minutes with seconds", () => {
+        render(<PrettyDuration seconds={60 * 4.5}/>)
+
+        expect(screen.getByText('4m30s')).toBeInTheDocument()
+    })
+})
