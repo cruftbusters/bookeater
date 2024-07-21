fcc450ac6602130bafb57dbb6f81fe71ab5d2735
tyler <tyler@cruftbusters.com>
2024-07-21 13:18:24 -0700

    refactor and style headers

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -5,16 +5,14 @@ import styles from './index.module.scss'
 function App() {
   return (
     <>
-      <header className={cn(styles.dull, styles.gap_padding)}>
+      <header className={cn(styles.dull, styles.header0, styles.gap_padding)}>
         Bookeater Timekeeping Tool
       </header>
       <p aria-label="readme" className={styles.gap_margin}>
         the plans for Bookeater Timekeeper Tool (BTT) are to try lots of things
         and get good with a vite typescript react neovim workflow
       </p>
-      <div className={cn(styles.gap_margin)}>
-        <DailyRowsDemo />
-      </div>
+      <DailyRowsDemo />
     </>
   )
 }
