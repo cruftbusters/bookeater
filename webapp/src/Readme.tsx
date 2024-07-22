cac8a546adc87668f5452de7627c3d4ac785b4dc
tyler <tyler@cruftbusters.com>
2024-07-21 20:09:13 -0700

    refactor readme

--- webapp/src/Readme.tsx
+++ webapp/src/Readme.tsx
@@ -2,10 +2,16 @@ import styles from './index.module.scss'
 
 function Readme() {
   return (
-    <p aria-label="readme" className={styles.gap_margin}>
-      the plans for Bookeater Timekeeper Tool (BTT) are to try lots of things
-      and get good with a vite typescript react neovim workflow
-    </p>
+    <div aria-label="readme">
+      {[
+        <>
+          the plans for Bookeater Timekeeper Tool (BTT) are to try lots of
+          things and get good with a vite typescript react neovim workflow
+        </>,
+      ].map((text, key) => (
+        <p key={key} className={styles.gap_margin}>{text}</p>
+      ))}
+    </div>
   )
 }
 
