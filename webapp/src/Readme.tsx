29fda303fc5b83d902967ed5019a3fac79a6f9b0
tyler <tyler@cruftbusters.com>
2024-07-21 20:12:13 -0700

    elaborate in the readme

--- webapp/src/Readme.tsx
+++ webapp/src/Readme.tsx
@@ -5,11 +5,21 @@ function Readme() {
     <div aria-label="readme">
       {[
         <>
-          the plans for Bookeater Timekeeper Tool (BTT) are to try lots of
-          things and get good with a vite typescript react neovim workflow
+          Bookeater Timekeeper Tool (BTT) simplifies recording and reporting of
+          time spent on an assignment
+        </>,
+        <>
+          the plan for BTT is to try lots of things and get good with a vite
+          typescript react neovim workflow
+        </>,
+        <>
+          this page is gravitating towards an interactive notebook. alongside
+          the "Daily Rows Demo" new and related demos may appear
         </>,
       ].map((text, key) => (
-        <p key={key} className={styles.gap_margin}>{text}</p>
+        <p key={key} className={styles.gap_margin}>
+          {text}
+        </p>
       ))}
     </div>
   )
