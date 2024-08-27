bd1187c61ca8d5e4143ea9b2dae6e8ca55bb15b8
tyler <tyler@cruftbusters.com>
2024-08-27 16:14:53 -0700

    move header into readme

--- apps/webapp/src/Readme.tsx
+++ apps/webapp/src/Readme.tsx
@@ -1,27 +1,33 @@
+import cn from './cn'
 import styles from './index.module.scss'
 
 function Readme() {
   return (
-    <div aria-label="readme">
-      {[
-        <>
-          Bookeater Timekeeper Tool (BTT) simplifies recording and reporting of
-          time spent on an assignment
-        </>,
-        <>
-          the plan for BTT is to try lots of things and get good with a vite
-          typescript react neovim workflow
-        </>,
-        <>
-          this page is gravitating towards an interactive notebook. alongside
-          the demo below new and related demos may appear
-        </>,
-      ].map((text, key) => (
-        <p key={key} className={styles.gap_margin}>
-          {text}
-        </p>
-      ))}
-    </div>
+    <>
+      <header className={cn(styles.dull, styles.header0, styles.gap_padding)}>
+        Bookeater Timekeeping Tool
+      </header>
+      <div aria-label="readme">
+        {[
+          <>
+            Bookeater Timekeeper Tool (BTT) simplifies recording and reporting
+            of time spent on an assignment
+          </>,
+          <>
+            the plan for BTT is to try lots of things and get good with a vite
+            typescript react neovim workflow
+          </>,
+          <>
+            this page is gravitating towards an interactive notebook. alongside
+            the demo below new and related demos may appear
+          </>,
+        ].map((text, key) => (
+          <p key={key} className={styles.gap_margin}>
+            {text}
+          </p>
+        ))}
+      </div>
+    </>
   )
 }
 
