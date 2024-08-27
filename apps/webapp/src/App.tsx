874906673d748e6928a86964a31e02d15a707e10
tyler <tyler@cruftbusters.com>
2024-08-27 16:14:53 -0700

    move header into readme

--- apps/webapp/src/App.tsx
+++ apps/webapp/src/App.tsx
@@ -1,15 +1,10 @@
-import cn from './cn'
 import PunchCardDemo from './PunchCardDemo'
-import styles from './index.module.scss'
 import Readme from './Readme'
 import BooksDemo from './BooksDemo'
 
 function App() {
   return (
     <>
-      <header className={cn(styles.dull, styles.header0, styles.gap_padding)}>
-        Bookeater Timekeeping Tool
-      </header>
       <Readme />
       <PunchCardDemo />
       <BooksDemo />
