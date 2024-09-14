f3c094592a8102726a7bbea65771580f4003bf8e
tyler <tyler@cruftbusters.com>
2024-09-14 14:18:11 -0700

    move planning into landing frontend

--- frontends/landing/src/App.tsx
+++ frontends/landing/src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Planning />
     </>
+  )
+}
+
+function Planning() {
+  return (
+    <div className={"large_margin_around"}>
+      <h1>planning</h1>
+      <h2>todo</h2>
+      <ul>
+        <li>playwright</li>
+        <li>
+          {
+            "add timekeeping { assignment spans of ( { start stop } duration() ) }"
+          }
+        </li>
+        <li>convert assignment to bookkeeping entry</li>
+        <li>vitest</li>
+        <li>deploy to cdn</li>
+      </ul>
+    </div>
   )
 }
 
