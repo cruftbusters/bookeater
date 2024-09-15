be32a27fa4ddbac8c1ac64743560eec6b75376a4
arctair <jtyler_@outlook.com>
2024-09-15 09:29:46 -0700

    planning

--- frontends/landing/src/Planning.tsx
+++ frontends/landing/src/Planning.tsx
@@ -4,12 +4,13 @@ export function Planning() {
       <h1>planning</h1>
       <h2>todo</h2>
       <ul>
-        <li>
-          {
-            'add timekeeping { assignment spans of ( { start stop } duration() ) }'
-          }
-        </li>
-        <li>convert assignment to bookkeeping entry</li>
+        <li>type assignment name</li>
+        <li>click to punch in</li>
+        <li>click to punch out</li>
+        <li>summarize duration by punch</li>
+        <li>summarize total duration by assignment</li>
+        <li>summarize total duration</li>
+        <li>convert timelog to bookkeeping entry</li>
         <li>vitest</li>
         <li>deploy to cdn</li>
       </ul>
