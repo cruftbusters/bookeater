19e6fbaa12b13003a7892aee4c850b7676394b23
arctair <jtyler_@outlook.com>
2024-09-15 09:26:23 -0700

    refactor to extract planning component

--- frontends/landing/src/App.tsx
+++ frontends/landing/src/App.tsx
@@ -1,27 +1,10 @@
+import { Planning } from './Planning'
+
 function App() {
   return (
     <>
       <Planning />
     </>
-  )
-}
-
-function Planning() {
-  return (
-    <div className={"large_margin_around"}>
-      <h1>planning</h1>
-      <h2>todo</h2>
-      <ul>
-        <li>
-          {
-            "add timekeeping { assignment spans of ( { start stop } duration() ) }"
-          }
-        </li>
-        <li>convert assignment to bookkeeping entry</li>
-        <li>vitest</li>
-        <li>deploy to cdn</li>
-      </ul>
-    </div>
   )
 }
 
---
+++ frontends/landing/src/Planning.tsx
@@ -0,0 +1,18 @@
+export function Planning() {
+  return (
+    <div className={'large_margin_around'}>
+      <h1>planning</h1>
+      <h2>todo</h2>
+      <ul>
+        <li>
+          {
+            'add timekeeping { assignment spans of ( { start stop } duration() ) }'
+          }
+        </li>
+        <li>convert assignment to bookkeeping entry</li>
+        <li>vitest</li>
+        <li>deploy to cdn</li>
+      </ul>
+    </div>
+  )
+}
